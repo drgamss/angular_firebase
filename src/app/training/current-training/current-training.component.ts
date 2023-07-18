2ab55b80afe079eb1d7c77c0169de8f8caf0ed47
Dani Gamss <drgamss@yahoo.com>
2023-07-18 20:51:26 +0300

    lesson 49

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
+  ngOnInit(): void {
+    setInterval(() => {
+      this.progress = this.progress + 5;
+    }, 1000)
+  }
+
+  progress = 0; 
 
 }
